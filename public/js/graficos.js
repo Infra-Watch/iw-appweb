ffc1d11d7ce8ebf6174256f1d33d26392ced5c48
Bryan-Anthony <bryan.amoraes@sptech.school>
2025-10-31 12:09:23 -0300

    ⚡refactor: Arrumando gráficos dash | gráfico de monitoramento de componente

--- public/js/graficos.js
+++ public/js/graficos.js
@@ -57,7 +57,7 @@ let options_1 = {
     colors: ["#9900a3ff"],
     series: [{
         name: 'Temperatura CPU',
-        data: [10, 35, 30, 50, 82, 35, 65, 120],
+        data: [10, 25, 30, 22, 18, 15, 15, 39],
     }]
 };
 let chart_1 = new ApexCharts(grafico_1, options_1);
@@ -138,7 +138,7 @@ let options_3 = {
     },
     series: [{
         name: 'Mbps Enviados',
-        data: [50, 56, 45, 39, 52, 45, 55, 50],
+        data: [150, 156, 145, 139, 152, 145, 155, 150],
     }]
 };
 let chart_3 = new ApexCharts(grafico_3, options_3);
@@ -154,10 +154,96 @@ let options_4 = {
     },
     series: [{
         name: 'Mbps Recebidos',
-        data: [51, 54, 46, 49, 50, 55, 53, 50],
+        data: [151, 154, 146, 149, 150, 155, 153, 150],
     }]
 };
 let chart_4 = new ApexCharts(grafico_4, options_4);
 chart_4.render();
 
 
+// RAM
+// GRÁFICO GAUGE CHART RAM
+let gauge_ram = document.getElementById("chart-apex-gauge-ram");
+let options_gauge_ram = {
+    series: [76],
+    chart: {
+        type: 'radialBar',
+        offsetY: -30,
+        sparkline: {
+            enabled: true
+        }
+    },
+    plotOptions: {
+        radialBar: {
+            startAngle: -90,
+            endAngle: 90,
+            track: {
+                background: "#e7e7e7",
+                margin: 5,
+                dropShadow: {
+                    enabled: true,
+                    color: '#444',
+                    opacity: 1,
+                    blur: 2
+                }
+            },
+            dataLabels: {
+                name: {
+                    show: false
+                },
+                value: {
+                    offsetY: -2,
+                    fontSize: '22px'
+                }
+            }
+        }
+    },
+    grid: {
+        padding: {
+            top: -10
+        }
+    },
+    labels: ['Average Results'],
+    colors: ["#25bb00ff"]
+
+};
+let chart_gauge_ram = new ApexCharts(gauge_ram, options_gauge_ram);
+chart_gauge_ram.render();
+
+// GRÁFICO 5
+let grafico_5 = document.getElementById("chart-apex-uso-ram");
+let options_5 = {
+    chart: {
+        type: 'line',
+        width: 300,
+        height: 110,
+    },
+    series: [{
+        name: 'Em uso',
+        data: [5.1, 6.3, 6.3, 6.3, 6.3, 6.3, 6.3, 6.2],
+    }],
+    colors: ["#25bb00ff"]
+};
+let chart_5 = new ApexCharts(grafico_5, options_5);
+chart_5.render();
+
+// GRÁFICO 6
+let grafico_6 = document.getElementById("chart-apex-disp-ram");
+let options_6 = {
+    chart: {
+        type: 'line',
+        width: 300,
+        height: 110,
+    },
+    series: [{
+        name: 'Memória Disponível',
+        data: [3.9, 2.7, 2.7, 2.7, 2.7, 2.7, 2.7, 2.7],
+    }],
+    colors: ["#25bb00ff"]
+};
+let chart_6 = new ApexCharts(grafico_6, options_6);
+chart_6.render();
+
+
+// PACOTES - Mbps
+// GRÁFICO GAUGE Mbps
