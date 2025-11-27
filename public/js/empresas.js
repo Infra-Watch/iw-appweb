f2a904fa3a3cae010916df771cb9b71ed7739f60
davi-domingues <davi.domingues@sptech.school>
2025-11-27 20:56:25 -0300

    :sparkles: feat: gereciamento empresas | adiciona modal de edição e deleção

--- public/js/empresas.js
+++ public/js/empresas.js
@@ -1,4 +1,11 @@
 const listaEmpresas = document.getElementById('lista-empresas')
+const botaoFechar = document.getElementById('botao-fechar')
+
+window.addEventListener('load', () => {
+    exibirEmpresas();
+})
+
+botaoFechar.addEventListener('click', () => {modoVisualizacao()})
 
 function exibirEmpresas() {
     fetch('/empresas/buscarTodas'
@@ -29,7 +36,7 @@ function exibirEmpresas() {
                     <p style="width: 20%">${empresa.cnpj}</p>
                     <p style="width: 20%">${empresa.nome_representante}</p>
                     <p style="width: 20%">${empresa.chave_acesso_adm}</p>
-                    <p style="width: 10%" onclick="configurarEmpresa(${empresa.idEmpresa})"><i class="fa-solid fa-ellipsis"></i></p>
+                    <p style="width: 10%" onclick="configurarEmpresa(${empresa.idEmpresa})"><i class="fa-solid fa-arrow-up-right-from-square"></i></p>
                 </div>
                 </div>
 			`
@@ -40,8 +47,70 @@ function exibirEmpresas() {
 	})
 }
 
-window.addEventListener('load', () => {
-    exibirEmpresas();
-})
+function configurarEmpresa(idEmpresa){
+    const modal = document.getElementById("modal_config")
+	fetch(`/empresas/buscar/${idEmpresa}`
+		, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		}    
+	)
+	.then((response) => {
+		if (response.ok) {
+			return response.json()
+		} else {
+			exibeErro('Não foi possível exibir');
+			return response.text().then(texto => console.error(texto));
+		}
+	})
+	.then((json) => {
+		if(!json)return;
+		console.log(json)
+		let empresa = json[0][0];
+		let query_status = json[1];
+		console.log(empresa)
+		Object.entries(empresa).forEach(([key, value]) => {
+			let elem = document.getElementById(`${key}-editar`)
+			if (elem) {
+				elem.setAttribute('value', ((value==''||value==undefined)?'Não informado':value))
+				elem.setAttribute('disabled', true)
+			}
+		})
+	})
+	.catch((error) => {
+		console.error(error);
+	})
+
+    abrirModal(modal)
+}
+
+function modoEdicao() {
+	let inputs = document.getElementsByTagName('input')
+	let botoesEdicao = document.getElementsByClassName('botao-edicao')
+	let tituloModal = document.getElementById('titulo-modal')
+	let botaoEditar = document.getElementById('botao-editar')
+	
+	Array.from(inputs).forEach(elem => {elem.removeAttribute('disabled')})
+	Array.from(botoesEdicao).forEach(botao => {botao.style.display = 'block'})
+	tituloModal.textContent = 'Editar Empresa'
+	botaoEditar.setAttribute('onclick', 'modoVisualizacao()')
+	botaoEditar.setAttribute('class', 'fa-solid fa-eye')
+}
+
+function modoVisualizacao() {
+	let inputs = document.getElementsByTagName('input')
+	let botoesEdicao = document.getElementsByClassName('botao-edicao')
+	let tituloModal = document.getElementById('titulo-modal')
+	let botaoEditar = document.getElementById('botao-editar')
+	
+	console.log(botoesEdicao)
+	Array.from(inputs).forEach(elem => {elem.setAttribute('disabled', true)})
+	Array.from(botoesEdicao).forEach(botao => {botao.style.display = 'none'})
+	tituloModal.textContent = 'Ver Detalhes da Empresa'
+	botaoEditar.setAttribute('onclick', 'modoEdicao()')
+	botaoEditar.setAttribute('class', 'fa-solid fa-pen-to-square')
+}
 
 function exibeErro(str) {alert(str)}
