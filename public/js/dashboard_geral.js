b69e7043621ff42cfe5604277797948c89074abc
MariaLuizaCostaGoes <maria.goes@sptech.school>
2025-12-01 23:03:30 -0300

    :wrench: fix: dashboard geral | arruma o frontend da dashboard geral

--- public/js/dashboard_geral.js
+++ public/js/dashboard_geral.js
@@ -1,6 +1,6 @@
 const lista_maquinas = document.querySelector('.maquinas');
 const idEmpresa = sessionStorage.ID_EMPRESA;
-const intervalo = sessionStorage.INTERVALO_DIAS != undefined ? sessionStorage.INTERVALO_DIAS: 1;
+const intervalo = sessionStorage.INTERVALO_DIAS != undefined ? sessionStorage.INTERVALO_DIAS : 1;
 
 window.addEventListener('load', () => {
 	if (idEmpresa == undefined) {
@@ -11,7 +11,7 @@ window.addEventListener('load', () => {
 	}
 });
 
-function plotarDashboard () {
+function plotarDashboard() {
 	exibirMaquinas();
 	exibirKpis();
 	exibirAlertas();
@@ -26,37 +26,36 @@ function exibirKpis() {
 				'Content-Type': 'application/json',
 			}
 		}
-		)
-			.then((resposta) => {
-				if (resposta.ok) {
-					return resposta.json();
-				} else {
-					exibeErro('Não foi possível exibir KPIs');
-					return resposta.text().then(texto => console.error(texto));
-					
-				}
-			})
-			.then((json) => {
-				if(!json)return;
-				let kpis = json[0][0];
-				let query_status = json[1];
-				document.getElementById("kpi-maquinas-ativas").textContent=`${kpis.maquinas_ativas}/${kpis.maquinas_totais}`
-				document.getElementById("kpi-trafego-total").textContent=`${kpis.trafego_total_24h} Kbps`
-				document.getElementById("kpi-maquina-critica").textContent=`${kpis.nome_maquina}`
-				document.getElementById("qtd_ultimos_alertas").textContent=`${kpis.total_alertas}`
-			})
-			.catch((erro) => {
-				console.error(erro);
-			});
+	)
+		.then((resposta) => {
+			if (resposta.ok) {
+				return resposta.json();
+			} else {
+				exibeErro('Não foi possível exibir KPIs');
+				return resposta.text().then(texto => console.error(texto));
+			}
+		})
+		.then((json) => {
+			if (!json) return;
+			let kpis = json[0][0];
+			let query_status = json[1];
+			document.getElementById("kpi-maquinas-ativas").textContent = `${kpis.maquinas_ativas}/${kpis.maquinas_totais}`
+			document.getElementById("kpi-trafego-total").textContent = `${kpis.trafego_total_24h} Kbps`
+			document.getElementById("kpi-maquina-critica").textContent = `${kpis.nome_maquina}`
+			document.getElementById("qtd_ultimos_alertas").textContent = `${kpis.total_alertas}`
+		})
+		.catch((erro) => {
+			console.error(erro);
+		});
 }
 function exibirMaquinas() {
 	fetch(`/maquinas/buscarPorEmpresa/${idEmpresa}`
-	, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
+		, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			}
 		}
-	}
 	)
 		.then((resposta) => {
 			if (resposta.ok) {
@@ -64,11 +63,10 @@ function exibirMaquinas() {
 			} else {
 				exibeErro('Não foi possível exibir máquinas');
 				return resposta.text().then(texto => console.error(texto));
-			
 			}
 		})
 		.then((json) => {
-			if(!json)return;
+			if (!json) return;
 			let maquinas = json[0];
 			let query_status = json[1];
 			if (maquinas.length > 0) {
@@ -78,17 +76,14 @@ function exibirMaquinas() {
 					let card = lista_maquinas.appendChild(document.createElement("article"));
 					card.setAttribute('class', 'card-maquina');
 					card.setAttribute('mac_address', maquina.mac_address);
-	
 					let cardTitle = card.appendChild(document.createElement("h3"));
 					cardTitle.textContent += maquina.nome_maquina;
 					cardTitle.innerHTML += elem_bolinha(alertas, ativacao);
-	
 					card.innerHTML += elem_status(alertas, ativacao);
 				});
 			} else {
 				let card = lista_maquinas.appendChild(document.createElement("article"));
 				card.setAttribute('class', 'card-maquina');
-				
 				let cardTitle = card.appendChild(document.createElement("h3"));
 				cardTitle.textContent = "Você ainda não possui máquinas monitoradas"
 			}
@@ -100,41 +95,41 @@ function exibirMaquinas() {
 
 function exibirAlertas() {
 	fetch(`/alertas/buscarPorEmpresa/${idEmpresa}/${intervalo}`
-	, {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-		}
-	})
-	.then((response) => {
-		if (response.ok) {
-			return response.json();
-		} else {
-			exibeErro('Não foi possível exibir alertas');
-			return response.text().then(texto => console.error(texto));
-		}
-	})
-	.then((json) => {
-		if(!json)return;
-		let alertas = json[0];
-		let query_status = json[1];
-		alertas.forEach((alerta) => {
-			document.getElementById('lista-alertas').innerHTML += `
-            <article class="alerta">
-              <p id="alerta_${alerta.idAlerta}">
-                Máquina: <span>${alerta.maquina}</span> <br>
-                Nível: <span style="color: ${cor_alerta(alerta.nivel_num)};">${alerta.nivel_label}</span> <br>
-                Componente: <span>${alerta.componente}</span> <br>
-                Registro: <span>${alerta.leitura}</span> <br>
-                Horário: <span>${dataFormatada(alerta.data_hora)}</span>
-              </p>
-            </article>
-			`
+		, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			}
 		})
-	})
-	.catch((error) => {
-		console.error(error);
-	})
+		.then((response) => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				exibeErro('Não foi possível exibir alertas');
+				return response.text().then(texto => console.error(texto));
+			}
+		})
+		.then((json) => {
+			if (!json) return;
+			let alertas = json[0];
+			let query_status = json[1];
+			alertas.forEach((alerta) => {
+				document.getElementById('lista-alertas').innerHTML += `
+<article class="alerta">
+<p id="alerta_${alerta.idAlerta}">
+Máquina: <span>${alerta.maquina}</span> <br>
+Nível: <span style="color: ${cor_alerta(alerta.nivel_num)};">${alerta.nivel_label}</span> <br>
+Componente: <span>${alerta.componente}</span> <br>
+Registro: <span>${alerta.leitura}</span> <br>
+Horário: <span>${dataFormatada(alerta.data_hora)}</span>
+</p>
+</article>
+`
+			})
+		})
+		.catch((error) => {
+			console.error(error);
+		})
 }
 
 function elem_bolinha(qtd_alertas, ativacao) {
@@ -144,9 +139,9 @@ function elem_bolinha(qtd_alertas, ativacao) {
 	} else if (qtd_alertas > 4) {
 		str = `<img class="status-bolinha" src="assets/imagens/circulo-vermelha.png" alt="Status Crítico">`
 	} else if (qtd_alertas > 2) {
-		str =  `<img class="status-bolinha" src="assets/imagens/circulo-amarelo.png" alt="Status Atenção">`
+		str = `<img class="status-bolinha" src="assets/imagens/circulo-amarelo.png" alt="Status Atenção">`
 	} else {
-		str =  `<img class="status-bolinha" src="assets/imagens/circulo-verde.png" alt="Status Regular">`
+		str = `<img class="status-bolinha" src="assets/imagens/circulo-verde.png" alt="Status Regular">`
 	}
 	return str
 };
@@ -155,27 +150,27 @@ function elem_status(qtd_alertas, ativacao) {
 	if (!ativacao) {
 		str = `<p>Status: <span id="stts_inativo">Inativo</span></p>`
 	} else if (qtd_alertas > 4) {
-		str =  `<p>Status: <span id="stts_critico">Crítico</span></p>`
+		str = `<p>Status: <span id="stts_critico">Crítico</span></p>`
 	} else if (qtd_alertas > 2) {
-		str =  `<p>Status: <span id="stts_perigo">Atenção</span></p>`
+		str = `<p>Status: <span id="stts_perigo">Atenção</span></p>`
 	} else {
-		str =  `<p>Status: <span id="stts_ok">Regular</span></p>`
+		str = `<p>Status: <span id="stts_ok">Regular</span></p>`
 	}
 	return str
 };
 
-function cor_alerta(nivel){
+function cor_alerta(nivel) {
 	if (nivel == 2) {
 		return 'red'
 	}
 	if (nivel == 1) {
-		return 'yellow'
+		return '#faa11c'
 	}
 }
 
-function dataFormatada (dataString) {
+function dataFormatada(dataString) {
 	const data = new Date(dataString);
 	return data.toLocaleString();
 }
 
-function exibeErro(str) {alert(str)}
+function exibeErro(str) { alert(str) }
--- src/routes/disco.js
+++ src/routes/disco.js
@@ -3,12 +3,10 @@ var router = express.Router();
 
 var discoController = require("../controllers/discoController");
 
-// Rota para buscar todos os KPIs de disco para uma máquina específica
 router.get("/kpis/:idEmpresa/:idMaquina", function(req, res) {
     discoController.pegarKpis(req, res);
 });
 
-// Rota para buscar os dados de uso de disco para o gráfico de série temporal
 router.get("/grafico_alertas/:idEmpresa/:idMaquina", function(req, res) {
     discoController.pegarDadosGraficoAlertas(req, res);
 });
